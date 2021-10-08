c5a2514b51133ba5db3a0cfbcb1af146d9e85475
RowenWitt <rew4200@protonmail.com>
2021-10-07 20:21:18 -0700

    makes changes

--- index.py
+++ index.py
@@ -6,6 +6,8 @@ class Indexer():
         self.add_to_table(body)
 
     def add_to_table(self, words):
+        ''' Adds sequences to index table
+        adds sequences to searchable body '''
         counter = 0
         for word in words:
             n_pos = len(self.body)
@@ -23,6 +25,8 @@ class Indexer():
         return counter
 
     def search_indexes(self, pattern):
+        ''' Search patterns and return indexes
+        of matches '''
         out = []
         for seq in pattern:
             if seq in self.table:
@@ -33,6 +37,8 @@ class Indexer():
         return out
 
     def search(self, pattern):
+        ''' Search patterns and return words
+        of matches '''
         indexes = [list(i) for i in self.search_indexes(pattern)]
         out = []
         for i in indexes:
@@ -40,6 +46,9 @@ class Indexer():
         return out
 
     def reindex(self, body=[]):
+        ''' Reindexes on an updated body,
+        returns number of steps taken for
+        triangular number check '''
         if body == []:
             body = self.body
         self.body = []
@@ -48,5 +57,6 @@ class Indexer():
         return count
 
     def delete(self, values):
+        ''' Reindexes without values to be deleted '''
         vals = [i for i in self.body if i not in values]
         self.reindex(vals)
--- test_index.py
+++ test_index.py
@@ -1,39 +1,27 @@
-import pytest
-import time
 import requests
-import sys
 from index import Indexer
 from utils import base_searcher
 
 
-# put benchmarking in own file
-
-# make method to diff all 4 methods
-# run test against the entire index to check for errors
-#
-links = {
-    "small": "https://www.gutenberg.org/files/11/11-0.txt",
-    "medium": "https://www.gutenberg.org/files/2701/2701-0.txt",
-    "large": "https://www.gutenberg.org/files/996/996-0.txt"
-}
+links = [
+    "https://www.gutenberg.org/files/11/11-0.txt",
+    "https://www.gutenberg.org/files/2701/2701-0.txt",
+    "https://www.gutenberg.org/files/996/996-0.txt"
+]
 
 
-def GetText(site):
+def get_text(site):
+    ''' Requests and cleans books from
+    Gutenberg website '''
     response = requests.get(site)
     words = response.text.replace('\n', '')
     words = words.replace('\r', ' ').split(' ')
     return words
 
-# GOAL TO PARAMETRIZE BIG DICT TESTS, BASE vs IND
-
-
-# @pytest.fixture
-# def alice_index(corpus):
-#     CP = Indexer(corpus)
-#     return CP
-
 
 def diff_patterns(pat_1, pat_2):
+    ''' Compares two lists, output will
+    have length of 0 if no difference '''
     for i in pat_1:
         if i in pat_2:
             pat_2.pop(pat_2.index(i))
@@ -42,7 +30,13 @@ def diff_patterns(pat_1, pat_2):
 
 
 def test_trinum():
-    corpus = GetText(links['small'])
+    ''' Compares the number of steps made
+    while reindexing, to the sum of the
+    triangular number of the length of each
+    string.  This equation tells you how many
+    subgroups can be found in a string
+        Xn = n(n + 1) / 2 '''
+    corpus = get_text(links[0])
     index = Indexer(corpus)
     found_trinum = index.reindex()
 
@@ -52,84 +46,47 @@ def test_trinum():
     assert t_nums == found_trinum
 
 
-def test_accuracy():
-    corpus = GetText(links['small'])
-    counter = 0
+def test_accuracy(per=100):
+    ''' Compares the results of the index search
+    to the example searcher.  The search terms are
+    the keys of the index storing the list data.
+    The per keyword determines the frequency of comparison.
+    per of 100 means every 100th key term is being compared.
+    There are 60,000 keys in the alice in wonderland index. '''
+    corpus = get_text(links[0])
     ind = Indexer(corpus)
     search_terms = list(ind.table.keys())
     mismatches = 0
-    for i in search_terms:
-        counter += 1
-        i_vals = ind.search([i])[0]
-        _, b_vals = base_searcher(corpus, [i])
+    for i in range(len(search_terms)):
+        if i % per == 0:
+            i_vals = ind.search([search_terms[i]])[0]
+            _, b_vals = base_searcher(corpus, [search_terms[i]])
 
-        a = i_vals.copy()
-        b = b_vals.copy()
-        dif_ind_base = diff_patterns(a, b)
+            a = i_vals.copy()
+            b = b_vals.copy()
+            dif_ind_base = diff_patterns(a, b)
 
-        mismatches += len(dif_ind_base)
+            mismatches += len(dif_ind_base)
 
     assert mismatches == 0
 
 
+def index_speed(corpus):
+    index = Indexer(corpus)
+    return index
 
 
-
-# For benchmarking V
-# --------------------------------
-
-# def test_index_speed(corpus):
-#     start = time.time()
-#     CP = Indexer(corpus)
-#     finish = time.time() - start
-#     size = sys.getsizeof(CP.table) * 1e-6
-#     print('Indexing took {} seconds with size of {} mb'.format(
-#         round(finish, 4),
-#         size
-#     ))
-
-#     assert finish < .5
-#     assert size < 3
+def test_index_speeds_small(benchmark):
+    ''' Tests index speed on 40k word
+    Alice in wonderland corpus '''
+    corpus = get_text(links[0])
+    val = benchmark(index_speed, corpus)
+    assert len(val.table) != 0
 
 
-# def benchmark_base(words, pattern):
-#     pattern_words = []
-#     start = time.time()
-#     for word in words:
-#         for pat in pattern:
-#             if pat in word:
-#                 pattern_words.append(word)
-#     duration = time.time() - start
-#     return duration, pattern_words
-
-
-# def benchmark_index(index, pattern):
-#     pattern_words = []
-#     start = time.time()
-#     words = index.search(pattern)
-#     duration = time.time() - start
-#     pattern_words = [i for j in words for i in j]
-#     return duration, pattern_words
-
-
-# def test_search_results_and_time(alice_index, corpus):
-#     index = alice_index
-#     corpus = corpus
-#     base_time_pat_1, b_1 = benchmark_base(corpus, pattern_1)
-#     base_time_pat_2, b_2 = benchmark_base(corpus, pattern_2)
-#     base_time_pat_3, b_3 = benchmark_base(corpus, pattern_3)
-
-#     index_time_pat_1, i_1 = benchmark_index(index, pattern_1)
-#     index_time_pat_2, i_2 = benchmark_index(index, pattern_2)
-#     index_time_pat_3, i_3 = benchmark_index(index, pattern_3)
-
-#     diff_pat_1 = diff_patterns(b_1, i_1)
-#     diff_pat_2 = diff_patterns(b_2, i_2)
-#     diff_pat_3 = diff_patterns(b_3, i_3)
-
-#     assert len(diff_pat_1) == 0
-#     assert len(diff_pat_2) == 0
-#     assert len(diff_pat_3) == 0
-
-#     assert base_time_pat_1 > index_time_pat_1
-#     assert base_time_pat_2 > index_time_pat_2
+def test_index_speeds_medium(benchmark):
+    ''' Test index speed on 200k word
+    Moby Dick Corpus '''
+    corpus = get_text(links[1])
+    val = benchmark(index_speed, corpus)
+    assert len(val.table) != 0
